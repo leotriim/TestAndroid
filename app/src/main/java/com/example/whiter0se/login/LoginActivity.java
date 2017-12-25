4c298f0fb4a947af1960b1f37bc3e03e9b339020
Leotrim Vojvoda <leo_triimi@live.com>
2017-12-25 15:39:45 +0100

    Initial Project Version

---
+++ app/src/main/java/com/example/whiter0se/login/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.whiter0se.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        //startActivity(intent);
+    }
+}
